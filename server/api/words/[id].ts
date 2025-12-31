cee5303cdea7de31426d8da122e3f2ed6bcad1b6
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-12-31 23:37:14 +0000

    add caching

--- server/api/words/[id].ts
+++ server/api/words/[id].ts
@@ -1,11 +1,21 @@
 export default defineEventHandler(async (event) => {
-  const id = getRouterParam(event, "id")
+
+
+ // max-age=3600	                Browser caches for 1 hour
+ // s-maxage=86400	              CDN caches for 24 hours
+ // stale-while-revalidate=600	  Serve stale for 10 mins while updating
+
+  setHeader(
+    event,
+    "cache-control",
+    "public, max-age=3600, s-maxage=86400, stale-while-revalidate=600"
+  );
+
+  const id = getRouterParam(event, "id");
   const origin = getRequestURL(event).origin;
-  console.log(`${origin}/content/cantonese/words/${id}.json`)
+
   try {
-    return await $fetch(
-      `${origin}/content/cantonese/words/${id}.json`
-    );
+    return await $fetch(`${origin}/content/cantonese/words/${id}.json`);
   } catch {
     throw createError({
       statusCode: 404,
