7fd5204b13b3fc27ac076cbe1b4e3a31e94928eb
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-12-31 22:16:42 +0000

    add one.json

--- server/api/words/[id].ts
+++ server/api/words/[id].ts
@@ -5,10 +5,10 @@ export default defineEventHandler(async (event) => {
   
   const id = getRouterParam(event, 'id')
 
-  const encodedParam = decodeURIComponent(id || "unable to decode character")  // TODO: we need to move to raw english and avoid encode and decoding
+  // const encodedParam = decodeURIComponent(id || "unable to decode character")  // TODO: we need to move to raw english and avoid encode and decoding
 
   try {
-    const file = join(process.cwd(), 'server/data/words', `${encodedParam}.json`)
+    const file = join(process.cwd(), 'server/data/words', `${id}.json`)
     const json = await readFile(file, 'utf-8')
     console.log(file)
     return JSON.parse(json)
