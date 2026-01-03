5ab0422e163035197a917e617a45dd072e528795
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2026-01-03 21:56:50 +0000

    wip changing topics server api to cdn

--- server/api/index/topics/[id].ts
+++ server/api/index/topics/[id].ts
@@ -1,25 +1,25 @@
 export default defineEventHandler(async (event) => {
-  // max-age=3600	                Browser caches for 1 hour
-  // s-maxage=86400	              CDN caches for 24 hours
-  // stale-while-revalidate=600	  Serve stale for 10 mins while updating
+  const id = getRouterParam(event, "id");
 
-  setHeader(
-    event,
-    "cache-control",
-    "public, max-age=3600, s-maxage=86400, stale-while-revalidate=600"
-  );
+  if (!id) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: "Missing id",
+    });
+  }
 
-  const id = getRouterParam(event, "id") || "no id or missing id";
-  const origin = getRequestURL(event).origin;
-
-  console.log(`${origin}/index/topics/${id}.json`);
+  const {
+    public: { cdnBase },
+  } = useRuntimeConfig();
 
   try {
-    return await $fetch(`${origin}/index/topics/${id}.json`);
+    return await $fetch(
+      `${cdnBase}/index/topics/${id}.json`
+    );
   } catch {
     throw createError({
       statusCode: 404,
-      statusMessage: "Word not found",
+      statusMessage: "Topic not found",
     });
   }
 });
